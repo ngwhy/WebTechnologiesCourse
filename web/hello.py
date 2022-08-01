3f33691f0ee56cbf2b9b38101b004a90fad4c9e9
I <try.tobe@yandex.ru>
2022-08-01 10:34:21 +0300

    my commit.

--- web/hello.py
+++ web/hello.py
@@ -1,5 +1,7 @@
 def wsgi_app(environ, start_response):
     
     start_response('200 OK', [('Content-type', 'text/html')])
+    
+    line = "\n".join(environ.get('QUERY_STRING').split("&"))
 
-    return ["\n".join(environ.get('QUERY_STRING').split("&"))]
+    return iter([line.encode('utf-8')])

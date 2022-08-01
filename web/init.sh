85d32d36164486cf8d8e580d6ffa66cd2ccdb694
I <try.tobe@yandex.ru>
2022-08-01 14:43:53 +0300

    AnaaaaCommit.

--- web/init.sh
+++ web/init.sh
@@ -1,5 +1,17 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -c hello.py hello:wsgi_app
+
+
+sudo ln -s /home/box/web/hello.py   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+
+
+cd /home/box/web/
+sudo gunicorn -c /home/box/web/etc/hello.py hello:wsgi_app
+
+
+
+
 
 

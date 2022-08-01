a8f3a58944ddfbd793ec850be0686267f6665d9c
I <try.tobe@yandex.ru>
2022-08-01 14:32:44 +0300

    AnCommit.

--- web/init.sh
+++ web/init.sh
@@ -1,16 +1,5 @@
-sudo rm /etc/nginx/sites-enabled/test.conf
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-
-sudo ln -s /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo /etc/init.d/gunicorn restart
-
-cd /home/box/web/
-gunicorn --bind='0.0.0.0:8080' hello:wsgi_app
-
-
-
-
+gunicorn -c hello.py hello:wsgi_app
 
 

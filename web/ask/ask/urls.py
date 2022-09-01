39d40ed630701bc6f6cc90de707b5b0e4325aff3
I <try.tobe@yandex.ru>
2022-09-01 16:34:28 +0300

    F commit.

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -13,15 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+
 from django.contrib import admin
+from django.conf.urls import url, include
+
+from qa import views
+
 
 urlpatterns = [
-    url(r'^$', include('qa.urls')),
-    url(r'^login/', include('qa.urls')),
-    url(r'^signup/', include('qa.urls')),
-    url(r'^question/.+/', include('qa.urls')),
-    url(r'^ask/', include('qa.urls')),
-    url(r'^popular/', include('qa.urls')), 
-    url(r'^new/', include('qa.urls'))
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^signup/$', views.test),
+    url(r'^question/\d+/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
 ]

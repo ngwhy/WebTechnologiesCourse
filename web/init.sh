98e01b68eccfe185c44a4ae88ce6aa618e8602eb
I <try.tobe@yandex.ru>
2022-09-01 14:31:11 +0300

    First commit.

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,10 @@
+sudo pip3 install virtualenv
+sudo pip3 install pathlib2
+virtualenv -p python3 venv
+source venv/bin/activate
+pip3 install django
+
+
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
